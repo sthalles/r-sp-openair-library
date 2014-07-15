39e5f99edcb784cf1c569bc2de818a801a45acb2
Thalles Silva <thalles753@gmail.com>
2014-07-15 12:52:36 -0400

    Adding OpenairSPfunctions.R, OpenAirHySplit.R and pheno2013 files

---
+++ OpenairSPfunctions.R
@@ -0,0 +1,338 @@
+library(openair)
+library(rgdal)
+library(sp)
+library(raster)
+library(plyr)
+
+# load the Canada's map
+#canada <- readOGR("/home/thalles/Desktop","province")
+
+# source('/home/thalles/OpenAirWD/openAirHySplit.R')
+# 
+# ########################
+# output = "pheno2012"
+# 
+# rimouski_N<-"48d27'N"
+# rimouski_W<-"68d32'W"
+# 
+# rimouski_north <- char2dms(rimouski_N)
+# rimouski_west <- char2dms(rimouski_W)
+# 
+# rimouski_decimal_coords <- c(as(rimouski_north, "numeric"), as(rimouski_west, "numeric"))
+# 
+# procTraj(lat = rimouski_decimal_coords[1], lon = rimouski_decimal_coords[2], year = 2013, name = output,
+#          start.month=07, start.day=15, end.month=07, end.day=17, hour.interval="1 hour",
+#          met = "/home/thalles/Desktop/hysplit/trunk/working/met2013/", out = "/home/thalles/OpenAirWD/", 
+#          hours = 3, height = 100, hy.path = "/home/thalles/Desktop/hysplit/trunk/") 
+# 
+# 
+# traj2013 <- importTraj(site = output, year = 2013, local="~/OpenAirWD/")
+# 
+# 
+# crs<-CRS("+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0")
+# t<-hySplit2SPDF(traj2013, crs)
+# 
+# 
+# spLines<-DF2SpLines(traj2013, crs) # create a SpatialLines object
+# spLinesDF<-DF2SLDF(spLines, traj2013, crs) # create a SpatialLinesDataFrame object
+# 
+# plotTraj(spLines, crs, axes=TRUE)
+# plotTrajFreq(spLinesDF, 20, 20, crs)
+# 
+# spplot(sp.lines, spLines)
+
+# Input: data frame
+# Output: SpatialPointsDataFrame
+hySplit2SPDF<-function(traj, crs)
+{
+    # get the coordinates out of the data.frame
+    cc <- traj[7:8]
+    
+    # reverse the order of the columns from [Lat Long] to [Long Lat]
+    cc<-cc[,c(2,1)]
+    
+    # get all the data (except) the coordinates
+    df <- traj[-7:-8]
+    
+    # create a SpatialPointsDataFrame object
+    SPDF <- SpatialPointsDataFrame(coords=cc, data=df)
+    
+    proj4string(SPDF)<-crs
+    SPDF
+}
+
+
+# DF2SLDF = DataFrame to SpatialLinesDataFrame
+# Input: SpatialLines, data frame, Projection
+# Output: SpatialLinesDataFrame
+DF2SLDF<-function( spLines, df, crs )
+{
+    # get the trajectory lenghth
+    # all trajectories have the same length
+    max.traj.length<-max(abs(df$hour.inc)) + 1
+    
+    # create a traj ID column to identify each trajctory uniquely 
+    df$ID<-rep(1:(nrow(df)/max.traj.length), each=max.traj.length )
+    
+    # apply the function to each subgroup of the data frame
+    # the dataframe is divided in subgroups of equal IDs (each trajectory has an unique ID)
+    # the function just get the fist line of each trajectory and returns a data.frame with that information
+    data.list<-ddply( df, .(ID), function(df){ df[1,] }, .inform=TRUE)
+
+    spLinesDataFrame<-SpatialLinesDataFrame(spLines, data=data.list)
+    proj4string(spLinesDataFrame)<-crs
+    return(spLinesDataFrame)
+}
+
+
+# DF2SL = Data Frame to SpatialLines
+# Input: DataFrame
+# Output: SpatialLines
+DF2SpLines<-function( df, crs, ... )
+{
+#     # get the number of cores available for parallel execution
+#     cores<-detectCores()
+#     
+#     # set up the parallel cluster
+#     cl <- startMPIcluster(count=cores)
+#     
+#     # register the cluster
+#     registerDoMPI(cl)
+#     
+#     # list to hold elements of type Lines
+#     linesList<-list()
+#     
+#     # make a group variable
+#     # get the length of the trajectories
+#     # all trajectories has the same length
+#     max.traj.length<-max(abs(df$hour.inc)) + 1
+#     
+#     # create a traj ID column to identify each trajctory uniquely 
+#     df$ID<-rep(1:(nrow(df)/max.traj.length), each=max.traj.length )
+# 
+#     # split the dataframe's elements based of its ID
+#     # each trajectory has its own ID
+#     dataFrames<-split(df, df$ID)  
+#     
+#     head(dataFrames)
+#     
+#     start.time<-Sys.time()
+#     
+#     linesList<-foreach( l=dataFrames, .combine='c', .packages="sp" ) %dopar% 
+#     {
+#         # get the coordinates out of the data.frame
+#         cc <- l[7:8]
+#         
+#         # reverse the order of the columns from [Lat Long] to [Long Lat]
+#         cc<-cc[,c(2,1)]
+#         
+#         # create a individual line
+#         line<-Line(cc)
+#         
+#         # transfor the line [line] into a Lines object and assign an unique ID
+#         Lines(line, ID=as.character(l$ID[1]))  
+#     }
+#     
+#     end.time<-Sys.time()
+#     time.taken<-end.time - start.time 
+#     time.taken
+#     
+#     closeCluster(cl)
+    
+    # get the number of phisical cores availables
+#     cores<-detectCores()
+#     
+#     cl <- makeCluster(cores)
+#     
+#     registerDoParallel(cl)
+
+#    start.time<-Sys.time()
+    
+    max.traj.length<-max(abs(df$hour.inc)) + 1
+    
+    if( nrow(df) %% max.traj.length != 0 )
+    {
+        stop("The number of rows in the 'df' argument is not a multiple of the length of a individual trajectory" )
+    }
+    
+    # create a traj ID column to identify each trajctory uniquely 
+    df$ID<-rep(1:(nrow(df)/max.traj.length), each=max.traj.length )
+    
+    createLines<-function(df)
+    {
+        #print(df)
+        # get the coordinates out of the data.frame
+        cc <- df[7:8]
+        
+        # reverse the order of the columns from [Lat Long] to [Long Lat]
+        cc<-cc[,c(2,1)]
+        
+        # create a individual line
+        line<-Line(cc)
+        
+        # transfor the line [line] into a Lines object and assign a unique ID
+        Lines(line, ID=as.character(df$ID[1]))  
+    }
+    
+    linesList<-dlply( df, .(ID), createLines)
+    
+    spLines<-SpatialLines(linesList)
+    proj4string(spLines)<-crs
+    
+#     end.time<-Sys.time()
+#     time.taken<-end.time - start.time 
+#     time.taken
+    
+#    stopCluster(cl)
+    
+    return(spLines)
+}
+
+
+plotBGmap<-function( traj, ... )
+{
+    # create the projection
+    hySplitProj<-CRS(proj4string(traj))
+    
+    # apply the projection
+    canada<-spTransform(canada,hySplitProj)
+    
+    # plot the canada map
+    plot(canada, border="white", col="lightgrey", add=TRUE, ... )
+}
+
+# plotTraj: The function plotTraj is designed to plot hySplit trajectories
+# with the initial location represented
+plotTraj<-function( traj, ... )
+{
+    #get the old par configuration
+    oldpar<-par(no.readonly=TRUE)
+    
+    # it reduces the margin's size
+    par(mar=c(0,0,0,0) + 2.0)
+    
+    # plot an empty plot with the proper axes
+    plot(spLines, axes=TRUE, type="n")
+    
+    # gets the bounding box of the object
+    # this information will help to focus on the trajectory
+    bb<-bbox(traj)
+    
+    plotBGmap(traj, xlim=bb[1,], ylim=bb[2,])
+    
+    # print the grid
+    grid(col="white")
+    
+    # plot the trajectory lines
+    plot(traj, add=TRUE, ...)
+    
+    # get the coordinates from the trajectory
+    cc<-coordinates(traj)
+    
+    # get the first coordinate point of the trajectory
+    origin <- cc[[1]][[1]][1,]
+    
+    # plot the trajectory initial point over the trajectory line
+    points(origin[1], origin[2], ...)
+    
+    box()
+    
+    # restore the par configuration
+    par(oldpar)
+}
+
+
+
+# Function responsable for ploting trajectory frequencies from SpatialObjects
+# Input: SpatialObject obs. Tested only with SpatialLines 
+#         gridX: X Dimenssion of the Grid
+#         gridY: Y Dimenssion of the Grid
+# output: plot a map of trajectory frequencies
+plotTrajFreq<-function( traj, resolution=10000, gridX=1, gridY=1, ... )
+{
+    pdf("test.pdf")
+    
+    #get the old par configuration
+    oldpar<-par(no.readonly=TRUE)
+    
+    # it reduces the margin's size
+    par(mar=c(0,0,0,0) + 2.0)
+    
+    # gets the bounding box of the object
+    # this information will help to focus on the trajectory
+    bb<-bbox(traj)
+    
+    # create raster object
+    rast <- raster(ncols=gridX, nrows=gridY)
+
+    extent(rast) <- extent(traj) # assigns the min and max latitude and longitude
+    
+    # set all the grids to NA
+    rast <- setValues(rast, NA)
+    
+    crs1 <- "+proj=aea +lat_1=46 +lat_2=60 +lat_0=44 +lon_0=-68.5 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0"
+    rast <- projectRaster(rast,crs=crs1,res=resolution)
+    
+    # get the projection from the sp object
+    crs2 <- proj4string(traj)
+    
+    # Reproject
+    rast <- projectRaster(rast,crs=crs2)
+    
+    # And then ... rasterize it! This creates a grid version 
+    # of your points using the cells of rast, values from the IP field:
+    rast2 <- rasterize(spLines, rast,  fun='count') 
+    
+    # plots only the box 
+    plot(rast2, legend=FALSE, alpha=0, 
+         col=colorRampPalette(c("light green", "yellow", "orange", "red"))(100) )
+
+    # plot the map on the background
+    plotBGmap(traj, xlim=bb[1,], ylim=bb[2,])
+    
+    #bb<-bbox(traj)
+    #plotBGmap(traj, xlim=bb[1,], ylim=bb[2,], CRS("+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0"))
+    plot(rast2, legend=TRUE, col=colorRampPalette(c("light green", "yellow", "orange", "red"))(100), alpha=0.8, axes=TRUE, add=TRUE) ## (n))
+    
+    # restore the par configuration
+    par(oldpar)
+    
+    dev.off()
+}
+
+splitSP<-function( sp, by )
+{
+    size<-length(sp)
+    
+    if ( by >= size )
+        stop("Error!")
+    
+    spList<-list()
+    
+    interval <- length(sp) %/% by
+    
+    # Serial Execution
+    count<-1
+    
+    for(i in seq(from=1, to = length(sp), by=interval)) 
+    {
+        if( count != by )
+        {
+            spList<-c(spList, (sp[i:(i+interval-1)]))
+            
+        }
+        else
+        {
+            spList<-c(spList, (sp[i:length(sp)]))
+            break
+        }
+        count<-count+1
+    }
+    
+    
+    spList
+}
+
+
+
+
---
+++ openAirHySplit.R
@@ -0,0 +1,241 @@
+####################################################
+####################################################
+####################################################
+
+script.name="test"
+
+read.files<-function(hours = 96, hy.path, ID) 
+{
+    combine.file.name<-paste("Rcombined_", ID, ".txt", sep="")
+    dump.file.name<-paste("tdump_", ID, "_", "*", sep="")
+	## find tdump files
+	files <- Sys.glob(dump.file.name)
+	output <- file(combine.file.name, 'w')
+
+  #print(files)
+  
+	## read through them all, ignoring 1st 7 lines
+	for (i in files)
+	{
+		input <- readLines(i)
+		input <- input[-c(1:7)] # delete header
+		writeLines(input, output)
+	}
+	close(output)
+
+	## read the combined txt file
+	traj <- read.table(paste0(hy.path, "working/", combine.file.name, sep=""), header = FALSE)
+	traj <- subset(traj, select = -c(V2, V7, V8))
+	
+	traj <- rename(traj, c(V1 = "receptor", V3 = "year", V4 = "month", V5 = "day",
+						   V6 = "hour", V9 = "hour.inc", V10 = "lat", V11 = "lon",
+						   V12 = "height", V13 = "pressure"))
+
+	## hysplit uses 2-digit years ...
+	year <- traj$year[1]
+
+	if (year < 50) 
+		traj$year <- traj$year + 2000 
+  else 
+    traj$year <- traj$year + 1900
+	
+
+	traj$date2 <- with(traj, ISOdatetime(year, month, day, hour, min = 0, sec = 0, tz = "EST")) # Setup the Canada, ON timezone
+	
+	## arrival time
+	traj$date <- traj$date2 - 3600 * traj$hour.inc
+	traj
+}
+
+
+
+add.met <- function(month, Year, met, bat.file, control.file) 
+{
+	## if month is one, need previous year and month = 12
+	if (month == 0) 
+	{
+		month <- 12
+		Year <- as.numeric(Year) - 1
+	}
+
+	if (month < 10) {
+		month <- paste("0", month, sep = "")
+	}
+  
+	## add first line
+	write.table(paste("echo", met, ">>", control.file, sep=" "), bat.file, col.names = FALSE, row.names = FALSE, quote = FALSE, append = TRUE)
+	
+	x <- paste("echo RP", Year, month, ".gbl >> ", control.file, sep = "")
+	
+	write.table(x, bat.file, col.names = FALSE, row.names = FALSE, quote = FALSE, append = TRUE)
+}
+
+# The procTraj function is designed for parallel execution
+procTraj <- function(lat = 51.5, lon = -0.1, year = 2010, start.month=05, start.day=01, end.month=05, 
+                     end.day=02, hour.interval="1 hour", name = "london",
+                     met = "c:/users/david/TrajData/", out = "c:/users/david/TrajProc/", 
+                     hours = 12, height = 100, hy.path = "/home/thalles/Desktop/hysplit/trunk/", ID) 
+{
+    # generate an unique CONTROL file name for each instance of HYSPLIT
+    control.file.number<-1 # this number is the extension of the CONTROL file
+    
+	## hours is the back trajectory time e.g. 96 = 4-day back trajectory
+	## height is start height (m)
+	lapply(c("openair", "plyr", "reshape2"), require, character.only = TRUE)
+
+	## function to run 12 months of trajectories
+	## assumes 96 hour back trajectories, 1 receptor
+	setwd(paste0(hy.path, "working/"))
+    
+    # insure that each HySplit process will create an individual script file
+    # which allows parallel processing 
+    bat.file.name<-paste(script.name, "_", ID, ".sh", sep="")
+    bat.file <- paste0(hy.path, "working/", bat.file.name) ## name of BAT file to add to/run
+	
+	start <- paste(year, start.month, start.day, sep = "-")
+	start <- paste(start, "00:00", sep = " ")
+	#print(start)
+    
+	end <- paste(year, end.month, end.day, sep = "-")
+	end <- paste(end, "23:00", sep = " ")
+    #print(end)
+	dates <- seq(as.POSIXct(start, "EST"), as.POSIXct(end, "EST"), by = "1 hour")
+
+    for (i in 1:length(dates)) 
+	{
+        control.file<-"CONTROL"
+        
+        # CONTROL FILE extension
+        # format: [1-9]+_[1-9]+...
+        # the first number represents the interation ID, it is necessary to insure that each
+        # thread will create a separate group of CONTROl.files
+        # the second number represents the iD of the trajectory, which can be determined
+        # by the number of trajectories for each individual point
+        control.file.extension<- paste(as.character(ID), "_", control.file.number, sep="")
+        
+        # create CONTROL file name
+        control.file<-paste(control.file, control.file.extension, sep=".")
+        
+		year <- format(dates[i], "%y")
+		Year <- format(dates[i], "%Y") # long format
+		month <- format(dates[i], "%m")
+		day <- format(dates[i], "%d")
+		hour <- format(dates[i], "%H")
+                
+        #print(hour)
+        
+        shbang <- "#!/bin/sh"
+    
+		write.table(shbang, bat.file, col.names = FALSE, row.names = FALSE, quote = FALSE)
+    
+		x <- paste("echo", year, month, day, hour, ">", control.file, sep=" ")
+		write.table(x, bat.file, col.names = FALSE, row.names = FALSE, quote = FALSE)
+
+		x <- paste("echo 1 >>", control.file, sep=" ")
+		write.table(x, bat.file, col.names = FALSE, row.names = FALSE, quote = FALSE, append = TRUE)
+		
+		x <- paste("echo", lat, lon, height, ">>", control.file, sep=" ")
+		write.table(x, bat.file, col.names = FALSE, row.names = FALSE, quote = FALSE, append = TRUE)
+		
+		x <- paste("echo", hours, ">>", control.file, sep=" ")
+		write.table(x, bat.file, col.names = FALSE, row.names = FALSE, quote = FALSE, append = TRUE)
+		
+        x<- paste("echo 0", ">>", control.file, "\n",
+                  "echo 10000.0 >>", control.file, "\n",
+                  "echo 3 >>", control.file, "\n",
+                  sep=" ")
+
+        write.table(x, bat.file, col.names = FALSE, row.names = FALSE, quote = FALSE, append = TRUE)
+
+		## processing always assumes 3 months of met for consistent tdump files
+		months <- as.numeric(unique(format(dates[i], "%m")))
+		months <- c(months, months + 1:2)
+		months <- months - 1 ## to make sure we get the start of the previous year
+		months <- months[months <= 12]
+        #print(months)
+		if (length(months) == 2) {
+			months <- c(min(months) - 1, months)
+		}
+
+        for (i in 1:3)
+			add.met(months[i], Year, met, bat.file, control.file)
+
+		x <- paste("echo ./ >>", control.file, sep=" ")
+		write.table(x, bat.file, col.names = FALSE, row.names = FALSE, quote = FALSE, append = TRUE)
+
+		x <- paste("echo tdump", "_", ID, "_", year, month, day, hour, " >> ", control.file, sep = "")
+		write.table(x, bat.file, col.names = FALSE, row.names = FALSE, quote = FALSE, append = TRUE)
+
+        x <- paste("/home/thalles/Desktop/hysplit/trunk/exec/hyts_std", control.file.extension, sep=" ")
+		write.table(x, bat.file, col.names = FALSE, row.names = FALSE, quote = FALSE, append = TRUE)
+		
+		## run the file
+		system(paste0("sh ", hy.path, 'working/', bat.file.name))
+        
+        # create another control file
+		control.file.number<-control.file.number+1
+	}
+	
+	## combine files and make data frame
+	traj <- read.files(hours, hy.path, ID)
+
+	## write R object to file
+	file.name <- paste(out, name, Year, ".RData", sep = "")
+	save(traj, file = file.name)
+    
+    #print("Files generated: ")
+	#print(length(dates))
+    
+    ################################################################################################
+    # clean up
+    ################################################################################################
+    
+#     path.files <- paste0(hy.path, "working/")
+#     
+#     ## remove existing "tdump" files
+#     files <- list.files(path = path.files, pattern = paste("tdump_", ID))
+#     lapply(files, function(x) file.remove(x))
+#     
+#     # remove existing CONTROL. and MESSAGE files
+#     files <- list.files(path = path.files, pattern = paste("CONTROL.", ID))
+#     lapply(files, function(x) file.remove(x))
+#     
+#     files <- list.files(path = path.files, pattern = paste("MESSAGE.", ID))
+#     lapply(files, function(x) file.remove(x))
+#     
+#     files <- list.files(path = path.files, pattern = paste("Rcombined_", ID))
+#     lapply(files, function(x) file.remove(x))
+#     
+#     # Delete all the script files
+#     files <- list.files(path = path.files, pattern = paste(script.name, "_", ID, sep=""))
+#     lapply(files, function(x) file.remove(x))
+    
+}
+
+
+# This function will clean up all the files created by the routine procTraj
+# the files that will be deleted are: tdump_, CONTROL., and MESSAGE.
+cleanWD<-function(hy.path = "/home/thalles/Desktop/hysplit/trunk/")
+{
+    ## remove existing "tdump" files
+    path.files <- paste0(hy.path, "working/")
+    
+    files <- list.files(path = path.files, pattern = "tdump_")
+    lapply(files, function(x) file.remove(x))
+    
+    # remove existing CONTROL. and MESSAGE files
+    files <- list.files(path = path.files, pattern = "CONTROL.")
+    lapply(files, function(x) file.remove(x))
+    
+    files <- list.files(path = path.files, pattern = "MESSAGE.")
+    lapply(files, function(x) file.remove(x))
+    
+    files <- list.files(path = path.files, pattern = "Rcombined_")
+    lapply(files, function(x) file.remove(x))
+    
+    # Delete all the script files
+    files <- list.files(path = path.files, pattern = paste(script.name, "_", sep=""))
+    lapply(files, function(x) file.remove(x))
+    
+    invisible(NA)
+}
